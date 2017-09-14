7b5a3f9a7c360f28db2857da3fc1cb22a179be15
Hans Fangohr <hans.fangohr@xfel.eu>
2017-09-15 00:16:03 +0200

    change filename of script

--- euxfel_h5tools/test_lib.py
+++ euxfel_h5tools/test_lib.py
@@ -6,7 +6,7 @@ import pytest
 def test_summary_on_data_file():
     testdatapath = "data/example_data/R0126-AGG01-S00002.h5"
     if os.path.exists(testdatapath):
-        output = str(subprocess.check_output("python euxfel_h5tools/xhstat.py {}".format(
+        output = str(subprocess.check_output("python euxfel_h5tools/euxfel_h5tool.py {}".format(
             testdatapath), shell=True))
         print(output)
         assert "Size: 665.596177 MB" in output
