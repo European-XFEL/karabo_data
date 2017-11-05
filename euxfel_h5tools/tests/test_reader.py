2074494112783c6435424b55dd21b4b65effb86c
Thomas Michelat <thomas.michelat@xfel.eu>
2017-11-05 18:18:55 +0100

    add file reader class, refactor library and tests structure

---
+++ euxfel_h5tools/tests/test_reader.py
@@ -0,0 +1,92 @@
+import pytest
+from time import sleep
+
+from euxfel_h5tools import H5File
+
+FILEPATH = './R0019-lpd00-S00000.h5'
+
+@pytest.fixture(scope="session")
+def file_opened():
+    """open and close hdf5 file"""
+
+    print("opening file: ", FILEPATH)
+    h5f = H5File(FILEPATH)
+
+    yield h5f
+
+    print("closing file...")
+    h5f.close()
+
+
+def test_get_file_infos(file_opened):
+    f = file_opened
+
+    trains = f.train_ids[()]
+    train_count = len(trains)
+    devices = f.devices
+    sources = f.sources
+
+    print('train ids:', trains)
+    print('count: ', train_count)
+    print('devices: ', devices)
+    print('data_sources: ', sources)
+
+    assert 1455918683 in trains
+    assert 1234 not in trains
+    assert train_count == 20
+    assert 'FXE_DET_LPD1M-1/DET/0CH0:xtdf/detector' in devices
+    assert 'INSTRUMENT/FXE_DET_LPD1M-1/DET/0CH0:xtdf/detector' in sources
+
+
+def test_iterate_trains(file_opened):
+    f = file_opened
+
+    for data, train_id, index in f.trains():
+        print(index, train_id)
+        assert index in range(0,20)
+        assert train_id in range(1455918683, 1455918703)
+        assert 'FXE_DET_LPD1M-1/DET/0CH0:xtdf' in data.keys()
+        assert len(data) == 1
+        assert len(data['FXE_DET_LPD1M-1/DET/0CH0:xtdf']) == 21
+
+
+def test_get_train_per_id(file_opened):
+    f = file_opened
+
+    data, train, idx = f.train_from_id(1455918700)
+    print(data)
+    assert data is not None
+    assert train == 1455918700
+    assert idx == 17
+
+    with pytest.raises(ValueError) as info:
+        data = f.train_from_id(1234)  # train id not in file
+
+
+def test_get_train_per_index(file_opened):
+    f = file_opened
+
+    data, train, idx = f.train_from_index(0)
+    assert data is not None
+    assert train == 1455918683
+    assert idx == 0
+
+    with pytest.raises(ValueError) as info:
+        data, _, _ = f.train_from_index(20)  # index out of range
+
+
+def test_read_metadata(file_opened):
+    f = file_opened
+
+    data1, _, _ = f.train_from_index(0)
+    sleep(2)
+    data2, _, _ = f.train_from_index(0)
+    assert 'metadata' in data1['FXE_DET_LPD1M-1/DET/0CH0:xtdf']
+    assert data1['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['metadata']['tid'] == 1455918683
+    assert data1['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['metadata']['sec'] != data2['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['metadata']['sec']
+    assert data1['FXE_DET_LPD1M-1/DET/0CH0:xtdf']['metadata']['source'] == 'FXE_DET_LPD1M-1/DET/0CH0:xtdf'
+
+
+if __name__ == '__main__':
+    pytest.main(["-v"])
+    print("Run 'py.test -v -s' to see more output")
--- euxfel_h5tools/tests/test_utils.py
+++ euxfel_h5tools/tests/test_utils.py
@@ -1,7 +1,10 @@
+import numpy as np
 import os
+import pytest
 import subprocess
 
-import pytest
+from euxfel_h5tools.utils import QuickView
+
 
 def test_summary_on_data_file():
     testdatapath = "data/example_data/R0126-AGG01-S00002.h5"
@@ -63,3 +66,23 @@ def test_cbf_conversion():
 
     else:
         pytest.skip("test data file not available ()".format(testdatapath))
+
+
+def test_init_quick_view():
+    qv = QuickView()
+
+    assert qv.data is None
+    qv.data = np.empty((1,1,1), dtype=np.int8)
+    assert len(qv) == 1
+    assert qv.pos == 0
+
+    with pytest.raises(TypeError) as info:
+        qv.data = 4
+
+    with pytest.raises(TypeError) as info:
+        qv.data = np.empty((1,1,1,1), dtype=np.int8)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v"])
+    print("Run 'py.test -v -s' to see more output")
