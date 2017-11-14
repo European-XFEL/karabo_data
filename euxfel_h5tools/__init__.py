1ade42168945b573c99ed7c66d75f5bba92e71d7
Thomas Michelat <tmichela@exflpcx17673.desy.de>
2017-11-14 16:31:43 +0100

    add handler class to get data per xray train from a run directory

--- euxfel_h5tools/__init__.py
+++ euxfel_h5tools/__init__.py
@@ -1,3 +1,3 @@
-from .reader import H5File
+from .reader import H5File, open_H5File, RunHandler
 from .utils import (hdf5_file_info, hdf5_paths, hdf5_to_cbf, numpy_to_cbf,
                     QuickView)
