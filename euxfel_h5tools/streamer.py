22157e66b22fb9f5d09cd0ac29e767b26592bcab
Thomas Michelat <thomas.michelat@xfel.eu>
2018-02-23 19:03:46 +0100

    blocking queue, improve thread management

--- euxfel_h5tools/streamer.py
+++ euxfel_h5tools/streamer.py
@@ -1,12 +1,47 @@
-from collections import deque
+"""Expose data to different interface
+
+ZMQStream explose to a ZeroMQ socket in a REQ/REP pattern.
+"""
+
+
+__all__ = ('ZMQStreamer',)
+
+
 import msgpack
-import msgpack_numpy
-from threading import Thread
-from time import sleep
+import msgpack_numpy as numpack
+from queue import Queue
+from threading import Event, Thread
 import zmq
 
 
-msgpack_numpy.patch()
+class REPInterface(Thread):
+    def __init__(self, context, port, buffer):
+        super(REPInterface, self).__init__()
+        self.context = context
+        self.port = port
+        self.buffer = buffer
+        self._stop_event = Event()
+
+    def run(self):
+        try:
+            interface = self.context.socket(zmq.REP)
+            interface.bind('tcp://*:{}'.format(self.port))
+
+            while not self.stopped():
+                req = interface.recv()
+                if req != b'next':
+                    raise RuntimeError('Unknown request:', req)
+                interface.send_multipart(self.buffer.get())
+        finally:
+            if interface:
+                interface.setsockopt(zmq.LINGER, 0)
+                interface.close()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
 
 
 class ZMQStreamer:
@@ -14,6 +49,8 @@ class ZMQStreamer:
 
         # server
         serve = ZMQStreamer(1234)
+        serve.start()
+
         for tid, data in run.trains():
             result = important_processing(data)
             serve.feed(result)
@@ -28,37 +65,34 @@ class ZMQStreamer:
     port: int
         Port to bind socket to.
     maxlen: int, optional
-        size of the buffer (old data is discarded if max len is reached)
-        default: infinite.
+        size of the buffer - default: 10.
     """
-    def __init__(self, port, maxlen=None):
+    def __init__(self, port, maxlen=10):
         self._context = zmq.Context()
-        self._socket = self._context.socket(zmq.REP)
-        self._socket.bind("tcp://*:{}".format(port))
+        self.port = port
+        self._buffer = Queue(maxsize=maxlen)
+        self._interface = None
 
-        self._buffer = deque(maxlen=maxlen)
-        self._rep = Thread(target=self._interface, args=())
-        self._rep.daemon = True
-        self._rep.start()
-
-    def _interface(self):
-        while True:
-            if len(self._buffer):
-                req = self._socket.recv()
-                if req != b'next':
-                    raise RuntimeError('Unknown request:', req)                
-                self._socket.send_multipart(self._buffer.popleft())
-            else:
-                sleep(0.1)
+    def start(self):
+        """Start a zmq.REP socket.
+        """
+        self._interface = REPInterface(self._context, self.port, self._buffer)
+        self._interface.daemon = True
+        self._interface.start()
 
     def stop(self):
-        raise NotImplementedError
+        if self._interface:
+            self._interface.stop()
+            self._interface.join()
+            self._interface = None
 
     def _serialize(self, data):
         # TODO: optimize this...
-        return [msgpack.dumps(data, use_bin_type=True)]
+        return [msgpack.dumps(data, use_bin_type=True, default=numpack.encode)]
 
     def feed(self, data):
         """Push data to the sending queue.
+
+        This call is blocking if the Queue is full.
         """
-        self._buffer.append(self._serialize(data))
+        self._buffer.put(self._serialize(data))
