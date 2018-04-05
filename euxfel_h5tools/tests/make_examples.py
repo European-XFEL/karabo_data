8e78f35b1a40a2a8e6ab3585a11f0653cd90101a
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-05 17:20:34 +0200

    Refactoring code to generate mock data

--- euxfel_h5tools/tests/make_examples.py
+++ euxfel_h5tools/tests/make_examples.py
@@ -130,199 +130,21 @@ def make_agipd_example_file(path):
                         (256,), 'u8', maxshape=(None,))  # Empty in example
 
 
-XGM_CONTROL_TOPICS = [
-    ('beamPosition/ixPos', 'f4'),
-    ('beamPosition/iyPos', 'f4'),
-    ('current/bottom/output', 'f4'),
-    ('current/bottom/rangeCode', 'i4'),
-    ('current/left/output', 'f4'),
-    ('current/left/rangeCode', 'i4'),
-    ('current/right/output', 'f4'),
-    ('current/right/rangeCode', 'i4'),
-    ('current/top/output', 'f4'),
-    ('current/top/rangeCode', 'i4'),
-    ('gasDosing/measuredPressure', 'f4'),
-    ('gasDosing/pressureSetPoint', 'f4'),
-    ('gasSupply/gasTypeId', 'i4'),
-    ('gasSupply/gsdCompatId', 'i4'),
-    ('pollingInterval', 'i4'),
-    ('pressure/dcr', 'f4'),
-    ('pressure/gasType', 'i4'),
-    ('pressure/pressure1', 'f4'),
-    ('pressure/pressureFiltered', 'f4'),
-    ('pressure/rd', 'f4'),
-    ('pressure/rsp', 'f4'),
-    ('pulseEnergy/conversion', 'f8'),
-    ('pulseEnergy/crossUsed', 'f4'),
-    ('pulseEnergy/gammaUsed', 'f4'),
-    ('pulseEnergy/gmdError', 'i4'),
-    ('pulseEnergy/nummberOfBrunches', 'f4'),
-    ('pulseEnergy/photonFlux', 'f4'),
-    ('pulseEnergy/pressure', 'f4'),
-    ('pulseEnergy/temperature', 'f4'),
-    ('pulseEnergy/usedGasType', 'i4'),
-    ('pulseEnergy/wavelengthUsed', 'f4'),
-    ('signalAdaption/dig', 'i4'),
-]
-
-XGM_INSTRUMENT_TOPICS = [
-    ('intensityTD', 'f4'),
-    ('intensityAUXTD', 'f4'),
-    ('intensitySigma/x_data', 'f4'),
-    ('intensitySigma/y_data', 'f4'),
-    ('xTD', 'f4'),
-    ('yTD', 'f4'),
-]
-
-def add_xgm_doocs(f, domain_id, N=400):
-    deviceId = domain_id + "/DOOCS/MAIN"
-    deviceId_output = domain_id + "/DOOCS/MAIN:output"
-
-    # INDEX
-    i_first = f.create_dataset('INDEX/%s/first' % deviceId,
-                               (N,), 'u8', maxshape=(None,))
-    i_count = f.create_dataset('INDEX/%s/count' % deviceId,
-                               (N,), 'u8', maxshape=(None,))
-    i2_first = f.create_dataset('INDEX/%s/data/first' % deviceId_output,
-                                (N,), 'u8', maxshape=(None,))
-    i2_count = f.create_dataset('INDEX/%s/data/count' % deviceId_output,
-                                (N,), 'u8', maxshape=(None,))
-    # This doesn't actually hold in all cases - a few trains don't have MAIN:output/data
-    i_first[:] = i2_first[:] = np.arange(N)
-    i_count[:] = i2_count[:] = 1
-
-    # CONTROL & RUN
-    # Creating empty datasets for now.
-    for (topic, datatype) in XGM_CONTROL_TOPICS:
-        f.create_dataset('CONTROL/%s/%s/timestamp' % (deviceId, topic),
-                         (N,), 'u8', maxshape=(None,))
-        f.create_dataset('CONTROL/%s/%s/value' % (deviceId, topic),
-                         (N,), datatype, maxshape=(None,))
-
-        # RUN is the value at the start of the run
-        f.create_dataset('RUN/%s/%s/timestamp' % (deviceId, topic),
-                         (1,), 'u8', maxshape=(None,))
-        f.create_dataset('RUN/%s/%s/value' % (deviceId, topic),
-                         (1,), datatype, maxshape=(None,))
-
-    # INSTRUMENT
-    write_train_ids(f, 'INSTRUMENT/%s/data/trainId' % deviceId_output,
-                    N, chunksize=200)
-    for (topic, datatype) in XGM_INSTRUMENT_TOPICS:
-        # 1000 cols, but only the first 30 have data in the example
-        f.create_dataset('INSTRUMENT/%s/data/%s' % (deviceId_output, topic),
-                         (N, 1000), datatype, maxshape=(None, 1000))
-
-
-GEC_CONTROL_TOPICS = [
-    ('acquisitionTime', 'f4'),
-    ('binningX', 'i4'),
-    ('binningY', 'i4'),
-    ('coolingLevel', 'i4'),
-    ('cropLines', 'i4'),
-    ('enableBiasCorrection', 'u1'),
-    ('enableBurstMode', 'u1'),
-    ('enableCooling', 'u1'),
-    ('enableCropMode', 'u1'),
-    ('enableExtTrigger', 'u1'),
-    ('enableShutter', 'u1'),
-    ('enableSync', 'u1'),
-    ('exposureTime', 'i4'),
-    ('firmwareVersion', 'i4'),
-    ('modelId', 'i4'),
-    ('numPixelInX', 'i4'),
-    ('numPixelInY', 'i4'),
-    ('numberOfCoolingLevels', 'i4'),
-    ('numberOfMeasurements', 'i4'),
-    ('pixelSize', 'f4'),
-    ('readOutSpeed', 'i4'),
-    ('shutterCloseTime', 'i4'),
-    ('shutterOpenTime', 'i4'),
-    ('shutterState', 'i4'),
-    ('syncHigh', 'u1'),
-    ('targetTemperature', 'i4'),
-    ('temperatureBack', 'f4'),
-    ('temperatureSensor', 'f4'),
-    ('triggerTimeOut', 'i4'),
-    ('updateInterval', 'i4'),
-]
-
-GEC_INSTRUMENT_TOPICS = [
-    ('image/bitsPerPixel', 'i4', ()),
-    ('image/dimTypes', 'i4', (2,)),
-    ('image/dims', 'u8', (2,)),
-    ('image/encoding', 'i4', ()),
-    ('image/pixels', 'u2', (255, 1024)),
-    ('image/roiOffsets', 'u8', (2,)),
-]
-
-def add_gec_camera(f, domain_id, N=400):
-    deviceId = domain_id + "/CAM/CAMERA"
-    deviceId_output = domain_id + "/CAM/CAMERA:daqOutput"
-
-    # INDEX
-    i_first = f.create_dataset('INDEX/%s/first' % deviceId,
-                               (N,), 'u8', maxshape=(None,))
-    i_count = f.create_dataset('INDEX/%s/count' % deviceId,
-                               (N,), 'u8', maxshape=(None,))
-    i_first[:] = np.arange(N)
-    i_count[:] = 1
-
-    i2_first = f.create_dataset('INDEX/%s/data/first' % deviceId_output,
-                                (N,), 'u8', maxshape=(None,))
-    i2_count = f.create_dataset('INDEX/%s/data/count' % deviceId_output,
-                                (N,), 'u8', maxshape=(None,))
-    # This pattern is an approximation of the real one
-    i2_first[:] = np.repeat(np.arange(N // 50), 50)
-    i2_count[::50] = 1
-
-    # CONTROL & RUN
-    # Creating empty datasets for now.
-    for (topic, datatype) in GEC_CONTROL_TOPICS:
-        f.create_dataset('CONTROL/%s/%s/timestamp' % (deviceId, topic),
-                         (N,), 'u8', maxshape=(None,))
-        f.create_dataset('CONTROL/%s/%s/value' % (deviceId, topic),
-                         (N,), datatype, maxshape=(None,))
-
-        # RUN is the value at the start of the run
-        f.create_dataset('RUN/%s/%s/timestamp' % (deviceId, topic),
-                         (1,), 'u8', maxshape=(None,))
-        f.create_dataset('RUN/%s/%s/value' % (deviceId, topic),
-                         (1,), datatype, maxshape=(None,))
-
-    # INSTRUMENT
-    tid = f.create_dataset('INSTRUMENT/%s/data/trainId' % deviceId_output,
-                           (200,), 'u8', maxshape=(None,))
-    tid[:N//50] = np.arange(10000, 10000 + N, 50, dtype='u8')
-    for (topic, datatype, dims) in GEC_INSTRUMENT_TOPICS:
-        f.create_dataset('INSTRUMENT/%s/data/%s' % (deviceId_output, topic),
-                         (200,) + dims, datatype, maxshape=((None,) + dims))
-    f['INSTRUMENT/%s/data/image/bitsPerPixel' % deviceId_output][:8] = 16
-    f['INSTRUMENT/%s/data/image/dims' % deviceId_output][:8] = [1024, 255]
 
 def make_fxe_da_file(path):
     """Make the structure of a file with non-detector data from the FXE experiment
 
     Based on .../FXE/201830/p900023/r0450/RAW-R0450-DA01-S00001.h5
     """
-    f = h5py.File(path, 'w')
+    from .mockdata import write_file
+    from .mockdata.xgm import XGM
+    from .mockdata.gec_camera import GECCamera
+    write_file(path, [
+        XGM('SA1_XTD2_XGM/DOOCS/MAIN', 400),
+        XGM('SPB_XTD9_XGM/DOOCS/MAIN', 400),
+        GECCamera('FXE_XAD_GEC/CAM/CAMERA', 400, nsamples=8)
+    ], ntrains=400, chunksize=200)
 
-    data_sources = [
-        'CONTROL/SA1_XTD2_XGM/DOOCS/MAIN',
-        'INSTRUMENT/SA1_XTD2_XGM/DOOCS/MAIN:output/data',
-        'CONTROL/SPB_XTD9_XGM/DOOCS/MAIN',
-        'INSTRUMENT/SPB_XTD9_XGM/DOOCS/MAIN:output/data',
-        'CONTROL/FXE_XAD_GEC/CAM/CAMERA',
-        'INSTRUMENT/FXE_XAD_GEC/CAM/CAMERA:daqOutput/data',
-    ]
-
-    make_metadata(f, data_sources, chunksize=200)
-
-    write_train_ids(f, 'INDEX/trainId', 400, chunksize=1)
-
-    add_xgm_doocs(f, 'SA1_XTD2_XGM')
-    add_xgm_doocs(f, 'SPB_XTD9_XGM')
-    add_gec_camera(f, 'FXE_XAD_GEC')
 
 if __name__ == '__main__':
     make_agipd_example_file('agipd_example.h5')
---
+++ euxfel_h5tools/tests/mockdata/__init__.py
@@ -0,0 +1,1 @@
+from .base import write_file
