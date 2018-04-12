20e0747788ea26f65b403433c31829c022b43693
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-12 12:22:29 +0200

    Separate file for tests reading mock data

---
+++ euxfel_h5tools/tests/test_reader_mockdata.py
@@ -0,0 +1,39 @@
+import os.path as osp
+import pytest
+from tempfile import TemporaryDirectory
+
+from euxfel_h5tools import (H5File, RunHandler, stack_data,
+                            stack_detector_data)
+from . import make_examples
+
+@pytest.fixture(scope='module')
+def mock_agipd_data():
+    with TemporaryDirectory() as td:
+        path = osp.join(td, 'CORR-R9999-AGIPD07-S00000.h5')
+        make_examples.make_agipd_example_file(path)
+        yield path
+
+@pytest.fixture(scope='module')
+def mock_fxe_control_data():
+    with TemporaryDirectory() as td:
+        path = osp.join(td, 'RAW-R0450-DA01-S00001.h5')
+        make_examples.make_fxe_da_file(path)
+        yield path
+
+def test_iterate_trains(mock_agipd_data):
+    with H5File(mock_agipd_data) as f:
+        for data, train_id, index in f.trains():
+            assert index in range(0, 250)
+            assert train_id in range(10000, 10250)
+            assert 'SPB_DET_AGIPD1M-1/DET/7CH0:xtdf' in data.keys()
+            assert len(data) == 1
+            assert 'image.data' in data['SPB_DET_AGIPD1M-1/DET/7CH0:xtdf']
+
+def test_iterate_trains_fxe(mock_fxe_control_data):
+    with H5File(mock_fxe_control_data) as f:
+        for data, train_id, index in f.trains():
+            assert index in range(0, 400)
+            assert train_id in range(10000, 10400)
+            assert 'SA1_XTD2_XGM/DOOCS/MAIN' in data.keys()
+            assert 'beamPosition.ixPos.value' in data['SA1_XTD2_XGM/DOOCS/MAIN']
+
