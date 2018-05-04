7fd241578cfd246e91301464a188bf6d3247ca5f
Thomas Kluyver <thomas@kluyver.me.uk>
2018-05-04 15:59:04 +0200

    Attempt at handling geometry data

--- karabo_data/geometry.py
+++ karabo_data/geometry.py
@@ -1,5 +1,7 @@
 import numpy as np
 import h5py, re
+import sys
+from textwrap import indent
 
 def splitChannelDataIntoTiles(channelData, clockwiseOrder=False):
     """Splits the raw channel data into indiviual tiles
@@ -174,3 +176,81 @@ def returnPositioned(geometry_file, modules, dquads):
             td = tileData[i][::-1,:,:]
             out[cx+x0:cx+x0+td.shape[0], cy+y0:cy+y0+td.shape[1], :] = td
     return out, len(smData), exceptions
+
+class GeometryFragment:
+    def __init__(self, offset, children):
+        self.offset = offset  # in m
+        self.children = children
+
+    def _str_lines(self):
+        r = []
+        for name, child in sorted(self.children.items()):
+            r.append("{}: {}".format(name, child.offset))
+            r.extend(indent(str(child), '  ').splitlines())
+        return r
+
+    def __str__(self):
+        return '\n'.join(self._str_lines())
+
+    def find_offset(self, name_parts):
+        if name_parts:
+            child = self.children[name_parts[0]]
+            return self.offset + child.find_offset(name_parts[1:])
+        return self.offset
+
+    @classmethod
+    def from_h5_group(cls, group, unit=1e-3):
+        children = {key: GeometryFragment.from_h5_group(val)
+                    for (key, val) in group.items()
+                    if isinstance(val, h5py.Group)
+                   }
+        return cls(group['Position'][:] * unit, children)
+
+class LPDGeometry(GeometryFragment):
+    pixel_size = 0.5e-3  # Meter: 0.5 Millimeter
+
+    @classmethod
+    def from_h5_file_and_quad_positions(cls, file, positions, unit=1e-3):
+        children = {}
+        for n, position in enumerate(positions, start=1):
+            quad = 'Q%d' % n
+            modules = {name: GeometryFragment.from_h5_group(group, unit=unit)
+                       for (name, group) in file[quad].items()}
+            children[quad] = GeometryFragment(np.asarray(position) * unit, modules)
+
+        return cls(np.asarray((0, 0)), children)
+
+    def position_all_modules(self, data):
+        data_1mod = list(data.values())[0]
+        out = np.zeros(data_1mod.shape[:-2] + (1500, 1500),
+                       dtype=data_1mod.dtype)
+        centre = np.asarray(out.shape[:2]) // 2
+
+        for module_ix in range(16):
+            try:
+                module_data = data[module_ix]
+            except KeyError:
+                continue
+
+            Q = 'Q{:d}'.format(module_ix // 4 + 1)
+            M = 'M{:d}'.format(module_ix % 4 + 1)
+            module_tiles_data = splitChannelDataIntoTiles(module_data)
+            for tileno, tile_data in enumerate(module_tiles_data, start=1):
+                T = "T{:02d}".format(tileno)
+                position = self.find_offset((Q, M, T))
+                # Convert physical position (m) to pixel location:
+                x0, y0 = (position / self.pixel_size) + centre
+
+                out[..., x0:x0 + tile_data.shape[0],
+                         y0:y0 + tile_data.shape[1]] = tile_data
+
+        return out
+
+
+if __name__ == '__main__':
+    quadpos = [(11.4, -299), (11.5, -8), (-254.5, 16), (-278.5, -275)]  # MAR 18
+    with h5py.File(sys.argv[1], 'r') as f:
+        geom = LPDGeometry.from_h5_file_and_quad_positions(f, quadpos, unit=1e-3)
+
+    print(geom)
+    print('Q2/M1/T07:', geom.find_offset(('Q2', 'M1', 'T07')))
