cce46afc45652feb0c65841e0d7c780e189e102e
Thomas Kluyver <thomas.kluyver@xfel.eu>
2018-08-09 17:06:44 +0200

    Clearer info in timing script

--- timing.py
+++ timing.py
@@ -15,10 +15,11 @@ train_iter = run.trains(devices=[('*/DET/*', 'image.data')], require_all=True)
 for i, (tid, data) in zip(range(1000), train_iter):
     assert data['SPB_DET_AGIPD1M-1/DET/0CH0:xtdf']['image.data'].shape[0] == 64
 arr = data['SPB_DET_AGIPD1M-1/DET/0CH0:xtdf']['image.data']
-print(arr.nbytes, "bytes per train")
+print(arr.nbytes * 16 / 1024, "KiB per train")  # * 16 detector modules
 delta = monotonic() - start
 print(delta, "seconds")
 
+print()
 print("Opening proc run...")
 start = monotonic()
 run = RunDirectory('/gpfs/exfel/exp/SPB/201830/p900022/proc/r0034')
@@ -33,6 +34,6 @@ train_iter = run.trains(devices=[('*/DET/*', 'image.data')], require_all=True)
 for i, (tid, data) in zip(range(1000), train_iter):
     assert data['SPB_DET_AGIPD1M-1/DET/0CH0:xtdf']['image.data'].shape[0] == 64
 arr = data['SPB_DET_AGIPD1M-1/DET/0CH0:xtdf']['image.data']
-print(arr.nbytes, "bytes per train")
+print(arr.nbytes * 16 / 1024, "KiB per train")
 delta = monotonic() - start
 print(delta, "seconds")
