e2da5ba496e5f0eff4722881852e659116c06c08
Thomas Michelat <thomas.michelat@xfel.eu>
2018-10-09 16:11:46 +0200

    add some tests

--- karabo_data/tests/test_reader_mockdata.py
+++ karabo_data/tests/test_reader_mockdata.py
@@ -1,4 +1,6 @@
 from itertools import islice
+
+import numpy as np
 import pandas as pd
 import pytest
 from xarray import DataArray
@@ -324,3 +326,62 @@ def test_stack_detector_data(mock_fxe_run):
 
     comb = stack_detector_data(data, 'image.data')
     assert comb.shape == (128, 1, 16, 256, 256)
+
+def test_stack_detector_data_missing(mock_fxe_run):
+    test_run = RunDirectory(mock_fxe_run)
+    tid, data = test_run.train_from_id(10000, devices=[('*/DET/*', 'image.data')])
+
+    missing = ['FXE_DET_LPD1M-1/DET/{}CH0:xtdf'.format(m) for m in (1, 5, 9, 15)]
+    for module in missing:
+        data[module]['image.data'] = np.zeros((0, 1, 256, 256), dtype=np.uint16)
+    
+    comb = stack_detector_data(data, 'image.data')
+    assert comb.shape == (128, 1, 16, 256, 256)
+    assert np.array_equal(comb[:, :, 5, ...],
+                           np.full((128, 1, 256, 256), np.nan, dtype=np.uint16))
+
+def test_stack_detector_data_wrong_pulses(mock_fxe_run):
+    test_run = RunDirectory(mock_fxe_run)
+    tid, data = test_run.train_from_id(10000, devices=[('*/DET/*', 'image.data')])
+
+    misshaped = ['FXE_DET_LPD1M-1/DET/{}CH0:xtdf'.format(m) for m in (12, 13)]
+    for module in misshaped:
+        data[module]['image.data'] = np.zeros((64, 1, 256, 256), dtype=np.uint16)
+    
+    with pytest.raises(ValueError) as excinfo:
+        comb = stack_detector_data(data, 'image.data')
+    assert '(64, 1, 256, 256)' in str(excinfo.value)
+
+def test_stack_detector_data_wrong_shape(mock_fxe_run):
+    test_run = RunDirectory(mock_fxe_run)
+    tid, data = test_run.train_from_id(10000, devices=[('*/DET/*', 'image.data')])
+
+    misshaped = ['FXE_DET_LPD1M-1/DET/{}CH0:xtdf'.format(m) for m in (0, 15)]
+    for module in misshaped:
+        data[module]['image.data'] = np.zeros((128, 1, 512, 128), dtype=np.uint16)
+    
+    with pytest.raises(ValueError) as excinfo:
+        comb = stack_detector_data(data, 'image.data')
+    assert '(128, 1, 512, 128)' in str(excinfo.value)
+
+def test_stack_detector_data_type_error(mock_fxe_run):
+    test_run = RunDirectory(mock_fxe_run)
+    tid, data = test_run.train_from_id(10000, devices=[('*/DET/*', 'image.data')])
+
+    module = 'FXE_DET_LPD1M-1/DET/3CH0:xtdf'
+    data[module]['image.data'] = data[module]['image.data'].astype(np.float32)
+    
+    with pytest.raises(ValueError) as excinfo:
+        comb = stack_detector_data(data, 'image.data')
+    assert "dtype('float32')" in str(excinfo.value)
+
+def test_stack_detector_data_extra_mods(mock_fxe_run):
+    test_run = RunDirectory(mock_fxe_run)
+    tid, data = test_run.train_from_id(10000, devices=[('*/DET/*', 'image.data')])
+
+    data.setdefault('FXE_DET_LPD1M-1/DET/16CH0:xtdf',
+                    {'image.data': np.zeros((128, 1, 256, 256), dtype=np.uint16)})
+    
+    with pytest.raises(IndexError) as excinfo:
+        comb = stack_detector_data(data, 'image.data')
+    assert "16" in str(excinfo.value)
