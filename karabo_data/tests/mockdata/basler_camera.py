e87bebc8b6fd84bbeda56d062a0f6869232bdda7
Martin Bergemann <martin.bergemann@xfel.eu>
2019-04-01 12:53:49 +0200

    Rename px_size -> sensor_size keyword

--- karabo_data/tests/mockdata/basler_camera.py
+++ karabo_data/tests/mockdata/basler_camera.py
@@ -9,9 +9,9 @@ class BaslerCamera(DeviceBase):
     Based on example /gpfs/exfel/exp/SPB/201930/p900061/raw/r0055/RAW-R0055-DA01-S00000.h5
     """
 
-    def __init__(self, device_id, nsamples=None, px_size=None):
+    def __init__(self, device_id, nsamples=None, sensor_size=None):
         """Create a dummy basler device that inherits from Device Base"""
-        self.px_size = px_size or (2058, 2456)
+        self.sensor_size = sensor_size or (2058, 2456)
         super(BaslerCamera, self).__init__(device_id, nsamples=nsamples)
         self.output_channels = ('daqOutput/data',)
         self.instrument_keys = [
@@ -19,7 +19,7 @@ class BaslerCamera(DeviceBase):
             ('image/dimTypes', 'i4', (2,)),
             ('image/dims', 'u8', (2,)),
             ('image/encoding', 'i4', ()),
-            ('image/pixels', 'u2', self.px_size),
+            ('image/pixels', 'u2', self.sensor_size),
             ('image/roiOffsets', 'u8', (2,)),
             ('image/binning', 'u8', (2,)),
             ('image/flipX', 'u1', ()),
@@ -33,6 +33,6 @@ class BaslerCamera(DeviceBase):
         for channel in self.output_channels:
             image_grp = 'INSTRUMENT/{}:{}/image/'.format(self.device_id, channel)
             f[op_join(image_grp, 'bitsPerPixel')][:self.nsamples] = 16
-            f[op_join(image_grp, 'dims')][:self.nsamples] = self.px_size
+            f[op_join(image_grp, 'dims')][:self.nsamples] = self.sensor_size
 
 
