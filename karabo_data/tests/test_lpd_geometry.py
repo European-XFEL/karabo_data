fa74c194c947b76a3666fb02d3a9239be7561a66
Martin Bergemann <martin.bergemann@xfel.eu>
2019-06-14 13:52:28 +0200

    Add crystfel tests

--- karabo_data/tests/test_lpd_geometry.py
+++ karabo_data/tests/test_lpd_geometry.py
@@ -1,3 +1,5 @@
+import os
+from cfelpyutils.crystfel_utils import load_crystfel_geometry
 import h5py
 from matplotlib.axes import Axes
 import numpy as np
@@ -7,6 +9,42 @@ from karabo_data.geometry2 import LPD_1MGeometry, invert_xfel_lpd_geom
 
 tests_dir = dirname(abspath(__file__))
 
+
+def test_write_read_crystfel_file(tmpdir):
+    geom = LPD_1MGeometry.from_quad_positions(
+        [(11.4, 299), (-11.5, 8), (254.5, -16), (278.5, 275)]
+    )
+    path = str(tmpdir / 'test.geom')
+    geom.write_crystfel_geom(filename=path)
+
+    with open(path, 'r') as f:
+        contents = f.read()
+    with open(path, 'w') as f:
+        f.write('clen = 0.119\n')
+        f.write('adu_per_eV = 0.0075\n')
+
+        f.write(contents)
+    # Load the geometry file with cfelpyutils and test the ridget groups
+    loaded = LPD_1MGeometry.from_crystfel_geom(path)
+    np.testing.assert_allclose(
+        loaded.modules[0][0].corner_pos, geom.modules[0][0].corner_pos
+    )
+    np.testing.assert_allclose(loaded.modules[0][0].fs_vec, geom.modules[0][0].fs_vec)
+
+
+    geom_dict = load_crystfel_geometry(path)
+    quad_gr0 = ['p0a0', 'p0a1', 'p0a2', 'p0a3', 'p0a4', 'p0a5', 'p0a6', 'p0a7',
+                'p0a8', 'p0a9', 'p0a10','p0a11', 'p0a12', 'p0a13', 'p0a14',
+                'p0a15', 'p1a0', 'p1a1','p1a2', 'p1a3','p1a4','p1a5','p1a6',
+                'p1a7', 'p1a8', 'p1a9', 'p1a10', 'p1a11', 'p1a12', 'p1a13',
+                'p1a14', 'p1a15', 'p2a0', 'p2a1', 'p2a2', 'p2a3', 'p2a4', 'p2a5',
+                'p2a6', 'p2a7', 'p2a8', 'p2a9', 'p2a10', 'p2a11', 'p2a12', 'p2a13',
+                'p2a14','p2a15', 'p3a0', 'p3a1','p3a2', 'p3a3', 'p3a4', 'p3a5',
+                'p3a6', 'p3a7', 'p3a8','p3a9', 'p3a10', 'p3a11', 'p3a12', 'p3a13',
+                'p3a14', 'p3a15']
+    assert geom_dict['rigid_groups']['p0'] == quad_gr0[:16]
+    assert geom_dict['rigid_groups']['p3'] == quad_gr0[-16:]
+    assert geom_dict['rigid_groups']['q0'] == quad_gr0
 
 def test_inspect():
     geom = LPD_1MGeometry.from_quad_positions(
