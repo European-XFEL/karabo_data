a42be1bd80571beeca4768d8dd3ade4cec81df88
Thomas Kluyver <thomas@kluyver.me.uk>
2019-08-05 10:38:59 +0100

    Version number -> 0.6

--- karabo_data/__init__.py
+++ karabo_data/__init__.py
@@ -33,7 +33,7 @@ You should have received a copy of the 3-Clause BSD License along with this
 program. If not, see <https://opensource.org/licenses/BSD-3-Clause>
 """
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 
 from .reader import *
